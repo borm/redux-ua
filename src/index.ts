0d222afa0db950be1ace6b00ffaac6ce883f906f
borm <efim4eg@gmail.com>
2021-02-20 19:10:44 +0200

    develop, new version

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,65 @@
-interface DecoratedFunction extends Function {
-  key: string;
-  type: string;
-  types: [];
-  isCompose: boolean;
-  isAsync: boolean;
-  isPure: boolean;
+interface BaseActionHandler {
+  isCompose?: boolean;
+  isAsync?: boolean;
+  isPure?: boolean;
 }
 
-export function createAction(key: string, handler: DecoratedFunction) {
+interface ActionHandler extends BaseActionHandler {
+  (...args: any[]): void | Thunk | {};
+}
+
+interface Action {
+  type: string;
+  payload: State;
+}
+
+interface Reducer {
+  (state: Record<string, State>, action: Action): State;
+}
+
+interface State {
+  loading?: boolean;
+  loaded?: boolean;
+  data?: any;
+  error?: any;
+}
+
+interface Thunk {
+  (
+    dispatch: (action: ActionHandler | Action) => void,
+    getState: () => Record<string, any>
+  ): Promise<any>;
+}
+
+interface DecoratedAction extends BaseActionHandler {
+  (...args: any[]): Thunk;
+  key: string;
+  type: string;
+  types: string[];
+  begin: string;
+  success: string;
+  failed: string;
+}
+
+enum ActionType {
+  BEGIN = 'BEGIN',
+  SUCCESS = 'SUCCESS',
+  FAILED = 'FAILED',
+}
+
+export function createAction(key: string, handler: ActionHandler) {
   const actionId = `${key.toUpperCase()}`;
-
-  const actionTypes = ['BEGIN', 'SUCCESS', 'FAILED'].map(
-    (type: string) => `${actionId}_${type}`
-  );
-
+  const actionTypes = [
+    ActionType.BEGIN,
+    ActionType.SUCCESS,
+    ActionType.FAILED,
+  ].map((type) => `${actionId}_${type}`);
   const [begin, success, failed] = actionTypes;
 
   const { isAsync, isPure, isCompose } = handler;
-
-  const action = (...args: any[]) => (
-    dispatch: ({}) => object,
-    getState: () => {}
+  const action: DecoratedAction = (...args: any[]) => async (
+    dispatch,
+    getState
   ) => {
     if (isAsync) {
       dispatch({
@@ -29,40 +68,38 @@ export function createAction(key: string, handler: DecoratedFunction) {
       });
     }
 
-    return new Promise(async (resolve, reject) => {
-      try {
-        let result = await handler(...args);
-        if (typeof result === 'function') {
-          result = await result(dispatch, getState);
-        }
-
-        if (isAsync) {
-          dispatch({
-            type: success,
-            payload: { loaded: true, loading: false, data: result },
-          });
-        }
-        if (isPure) {
-          dispatch({ type: actionId, payload: result });
-        }
-
-        if (isCompose) {
-          dispatch({ type: actionId, payload: result });
-        }
-        resolve(result);
-      } catch (error) {
-        if (isAsync) {
-          dispatch({
-            type: failed,
-            payload: { loaded: true, loading: false, error },
-          });
-        }
-        if (isPure) {
-          dispatch({ type: actionId, payload: error });
-        }
-        reject(error);
+    try {
+      let result: Function | any = await handler(...args);
+      if (typeof result === 'function') {
+        result = await result(dispatch, getState);
       }
-    });
+
+      if (isAsync) {
+        dispatch({
+          type: success,
+          payload: { loaded: true, loading: false, data: result },
+        });
+      }
+      if (isPure) {
+        dispatch({ type: actionId, payload: result });
+      }
+
+      if (isCompose) {
+        dispatch({ type: actionId, payload: result });
+      }
+      return result;
+    } catch (error) {
+      if (isAsync) {
+        dispatch({
+          type: failed,
+          payload: { loaded: true, loading: false, error },
+        });
+      }
+      if (isPure) {
+        dispatch({ type: actionId, payload: error });
+      }
+      return Promise.reject(error);
+    }
   };
 
   action.key = key;
@@ -81,57 +118,56 @@ export function createAction(key: string, handler: DecoratedFunction) {
   return action;
 }
 
-export function createActions(prefix: string, actions: { [key: string]: any }) {
-  return Object.keys(actions).reduce((accumulator: {}, key: string) => {
-    const actionId = `${prefix.toUpperCase()}__${key
-      .replace(/[A-Z]/g, '-$&')
-      .toUpperCase()}`;
+export function createActions(
+  prefix: string,
+  actions: Record<string, ActionHandler>
+) {
+  return Object.keys(actions).reduce(
+    (accumulator: { [key in keyof typeof actions]: DecoratedAction }, key) => {
+      const actionId = `${prefix.toUpperCase()}__${key
+        .replace(/[A-Z]/g, '-$&')
+        .toUpperCase()}`;
 
-    const action = createAction(actionId, actions[key]);
-    action.key = key;
+      const action = createAction(actionId, actions[key]);
+      action.key = key;
 
-    return {
-      ...accumulator,
-      [key]: action,
-    };
-  }, {});
+      return {
+        ...accumulator,
+        [key]: action,
+      };
+    },
+    {}
+  );
 }
 
 export function handleActions(
-  actions:
-    | {
-        [key: string]: DecoratedFunction;
-      }
-    | DecoratedFunction,
-  handlers: { [key: string]: any } = {},
-  initialState: { [key: string]: any } | null = {}
+  actions: Record<string, DecoratedAction> | DecoratedAction,
+  reducers: Record<string, Reducer> = {},
+  initialState: { [key: string]: any } = {}
 ) {
   const isFunction = typeof actions === 'function';
   let actionTypes: string[] = [];
 
-  const handleInitialState = (
-    key: string,
-    payload: { [key: string]: any } = {}
-  ) =>
+  const handleInitialState = (key: string, payload: Record<string, any> = {}) =>
     isFunction
       ? payload
       : {
-          ...initialState,
-          [key]: payload,
-        };
+        ...initialState,
+        [key]: payload,
+      };
 
   const handleState = (
     key: string,
-    state: { [key: string]: any } = {},
-    payload: { [key: string]: any } = {}
+    state: Record<string, any> = {},
+    payload: Record<string, any> = {}
   ) =>
     isFunction
       ? payload
       : {
-          ...state,
-          [key]: payload,
-        };
-  const handleAction = (key: string, action: DecoratedFunction) => {
+        ...state,
+        [key]: payload,
+      };
+  const handleAction = (key: string, action: DecoratedAction) => {
     const { isAsync, isPure, isCompose, type, types } = action;
     /**
      * Push actionTypes
@@ -149,7 +185,6 @@ export function handleActions(
      */
     if (typeof initialState[key] === 'undefined') {
       if (isAsync) {
-        // eslint-disable-next-line no-param-reassign
         initialState = handleInitialState(key, {
           loaded: false,
           loading: false,
@@ -158,8 +193,7 @@ export function handleActions(
       }
 
       if (isPure) {
-        // eslint-disable-next-line no-param-reassign
-        initialState = handleInitialState(key, null);
+        initialState = handleInitialState(key, {});
       }
     }
 
@@ -168,28 +202,20 @@ export function handleActions(
      */
     if (isAsync) {
       types.forEach((t) => {
-        if (!handlers[t]) {
-          // eslint-disable-next-line no-param-reassign
-          handlers[t] = (
-            state: { [key: string]: { loaded: boolean } },
-            { payload }: { payload: { loaded: boolean } }
-          ) =>
+        if (!reducers[t]) {
+          reducers[t] = (state, { payload }) =>
             handleState(key, state, {
               ...payload,
               loaded:
-                (isFunction ? state.loaded : state[key].loaded) ||
-                payload.loaded,
+                (isFunction ? state.loaded : state[key]?.loaded) ||
+                payload?.loaded,
             });
         }
       });
     }
 
-    if (isPure && !handlers[type]) {
-      // eslint-disable-next-line no-param-reassign
-      handlers[type] = (
-        state: { [key: string]: {} },
-        { payload }: { payload: {} }
-      ) => handleState(key, state, payload);
+    if (isPure && !reducers[type]) {
+      reducers[type] = (state, { payload }) => handleState(key, state, payload);
     }
   };
 
@@ -199,10 +225,10 @@ export function handleActions(
     Object.keys(actions).forEach((key) => handleAction(key, actions[key]));
   }
 
-  return (state = initialState, action: DecoratedFunction) => {
+  return (state = initialState, action: Action) => {
     let reducer;
     if (actionTypes.includes(action.type)) {
-      reducer = handlers[action.type];
+      reducer = reducers[action.type];
     }
 
     if (reducer) {
@@ -212,53 +238,21 @@ export function handleActions(
   };
 }
 
-export function compose(func: DecoratedFunction) {
+function decorator(func: ActionHandler, key: keyof ActionHandler) {
   if (func && typeof func === 'function') {
-    // eslint-disable-next-line no-param-reassign
-    func.isCompose = true;
+    func[key] = true;
     return func;
   }
   return (
-    target: Function,
+    target: ActionHandler,
     name: string,
-    descriptor: TypedPropertyDescriptor<any>
-  ) => {
-    // eslint-disable-next-line no-param-reassign
-    descriptor.value.isCompose = true;
-    return descriptor;
-  };
+    descriptor: TypedPropertyDescriptor<
+      { [key in keyof BaseActionHandler]: boolean }
+      >
+  ) => ({ ...descriptor, value: { [key]: true } });
 }
 
-export function async(func: DecoratedFunction) {
-  if (func && typeof func === 'function') {
-    // eslint-disable-next-line no-param-reassign
-    func.isAsync = true;
-    return func;
-  }
-  return (
-    target: Function,
-    name: string,
-    descriptor: TypedPropertyDescriptor<any>
-  ) => {
-    // eslint-disable-next-line no-param-reassign
-    descriptor.value.isAsync = true;
-    return descriptor;
-  };
-}
-
-export function pure(func: DecoratedFunction) {
-  if (func && typeof func === 'function') {
-    // eslint-disable-next-line no-param-reassign
-    func.isPure = true;
-    return func;
-  }
-  return (
-    target: Function,
-    name: string,
-    descriptor: TypedPropertyDescriptor<any>
-  ) => {
-    // eslint-disable-next-line no-param-reassign
-    descriptor.value.isPure = true;
-    return descriptor;
-  };
-}
+export const compose = (action: ActionHandler) =>
+  decorator(action, 'isCompose');
+export const async = (action: ActionHandler) => decorator(action, 'isAsync');
+export const pure = (action: ActionHandler) => decorator(action, 'isPure');
